ae0210b7fd53ee6db38ed5a42044755f4fd1919b
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2023-12-24 18:50:34 +0100

    Add image denoising class

---
+++ src/denoise/denoise.cpp
@@ -0,0 +1,54 @@
+#include <lightwave.hpp>
+#ifdef LW_WITH_OIDN
+  #include <OpenImageDenoise/oidn.hpp>
+#endif
+
+namespace lightwave {
+
+  class Denoise : public Postprocess {
+      ref<Image> input;
+      ref<Image> output;
+      ref<Image> normal;
+      ref<Image> albedo;
+
+  public:
+      Denoise(const Properties &properties): Postprocess(properties) {
+        input = m_input;
+        output = m_output;
+        normal = m_normals;
+        albedo = m_albedo;
+      }
+
+      void execute() override {
+        auto width = input->resolution().x();
+        auto height = input->resolution().y();
+        output->initialize(input->resolution());
+
+        oidn::DeviceRef device = oidn::newDevice(oidn::DeviceType::CPU);
+        device.commit();
+
+        oidn::FilterRef filter = device.newFilter("RT"); // generic ray tracing filter
+        filter.setImage("color", input->data(), oidn::Format::Float3, width, height); // beauty
+        filter.setImage("albedo", albedo->data(), oidn::Format::Float3, width, height);
+        filter.setImage("normal", normal->data(), oidn::Format::Float3, width, height);
+        filter.setImage("output", output->data(), oidn::Format::Float3, width, height); // denoised beauty
+        filter.set("hdr", true);
+        filter.commit();
+        filter.execute();
+
+        output->save();
+
+        const char* errorMessage;
+        if (device.getError(errorMessage) != oidn::Error::None) {
+          std::cout << "Error: " << errorMessage << std::endl;
+        }
+      }
+
+      std::string toString() const override {
+        return "Denoise[]";
+    }
+  };
+
+} // namespace lightwave
+
+REGISTER_POSTPROCESS(Denoise, "denoising")
