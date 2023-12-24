1adf1d60bc8c63f8777f9b9418cd102e6e731cc0
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2023-12-24 18:49:32 +0100

    Add albedo integrator

---
+++ src/integrators/albedo.cpp
@@ -0,0 +1,31 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+    class AlbedoIntegrator : public SamplingIntegrator {
+
+    public:
+        AlbedoIntegrator(const Properties &properties)
+        : SamplingIntegrator(properties) {}
+            
+        Color Li(const Ray &ray, Sampler &rng) override {
+            // Intersect the ray with a scene object
+            Intersection its = scene()->intersect(ray, rng);
+
+            if (its) {
+                return its.evaluateAlbedo();
+            }
+            
+            return Color(0);
+        }
+
+        std::string toString() const override {
+            return tfm::format(
+                "AlbedoIntegrator[]"
+            );
+        }
+    };
+}
+
+// this informs lightwave to use our class NormalsIntegrator whenever a <integrator type="normals" /> is found in a scene file
+REGISTER_INTEGRATOR(AlbedoIntegrator, "albedo")
